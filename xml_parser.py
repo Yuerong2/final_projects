103e83f3223c0f48714420049eaeff8a16f6765c
Tzu-Kun Hsiao <c9118061@gmail.com>
2019-11-18 00:14:03 -0600

    Update XML parser and news data.

--- xml_parser.py
+++ xml_parser.py
@@ -11,6 +11,7 @@ import re
 def clean_text(text, remove_digits=False):
     pattern = r'[^a-zA-Z0-9\s]' if not remove_digits else r'[^a-zA-Z\s]'
     text = re.sub(pattern, '', text)
+    text = text.replace('\t', ' ').replace('\n', ' ')
     text = ' '.join(text.split()).lower()
 
     return text
