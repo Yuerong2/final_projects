d0ba3aa9f89e23ed7aa39e2a5cbd7f602b9fb1f1
Yuerong <yuerong2@illinois.edu>
2019-12-07 16:03:27 -0600

    delete requirement.txt

--- ccLda/python.py
+++
@@ -1,9 +0,0 @@
-import os
-def cclda(file,iterations,topics,outputfile):
-   os.system('javac *.java')
-   os.system('java LearnTopicModel -model cclda -input {} -iters {} -Z {}'.format(file,iterations,topics))
-   output = file + '.assign'
-   os.system('python topwords_cclda.py {} > {}'.format(output,outputfile))
-
-os.system('rm *.class')
-cclda('input.txt',10,10,'output.txt')
